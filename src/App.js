e3951d4c332a1822309d26eed14868fcd6ecfc48
Ninfa Niño <ninfa_sys@hotmail.com>
2019-06-27 12:51:38 -0500

    landing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './components/header';
+import Projects from './components/projects';
+import Button from './components/button';
+import Mision from './components/mision';
+import Footer from './components/footer';
+import ModalContainer from './components/modalContainer';
+import Modal from './components/modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    modalVisible:false
+  }
+  openModal = () => {
+    this.setState({
+      modalVisible:true
+    })
+  }
+  closeModal = () => {
+    this.setState({
+      modalVisible:false
+    })
+  }
+
+  
+  render() {
+    var visits;
+    if (localStorage.getItem("visits") === null) {
+      localStorage.setItem("visits", 3885);
+      visits = Number(localStorage.getItem("visits"))
+    } else {
+      visits = Number(localStorage.getItem("visits")) + 1;
+      localStorage.setItem("visits", visits);
+    }
+    
+    return (
+      <div>
+        <Header visits={visits}/>
+        <Projects/>
+        <Button openModal={this.openModal}/>
+        <Mision/>
+        <Footer/>
+        {
+          this.state.modalVisible &&
+          <ModalContainer>
+            <Modal closeModal={this.closeModal}>
+
+            </Modal>
+          </ModalContainer>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
